e5a9dbcedd59ae73e279515f5996c63f8cf3babc
jamack <jamack9@hotmail.com>
2018-07-21 22:38:18 -0500

    Chapter 11 Challenge - "Adding First and Last Buttons"

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
@@ -6,8 +6,11 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.List;
 import java.util.UUID;
@@ -18,6 +21,11 @@ public class CrimePagerActivity extends AppCompatActivity {
 
     private ViewPager mViewPager;
     private List<Crime> mCrimes;
+
+    // For Challenge
+    private PagerAdapter mAdapter;
+    private Button mFirstButton;
+    private Button mLastButton;
 
     public static Intent newIntent(Context packageContext, UUID crimeId) {
         Intent intent = new Intent(packageContext, CrimePagerActivity.class);
@@ -36,7 +44,22 @@ public class CrimePagerActivity extends AppCompatActivity {
 
         mCrimes = CrimeLab.get(this).getCrimes();
         android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
-        mViewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
+
+//        mViewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
+//            @Override
+//            public Fragment getItem(int position) {
+//                Crime crime = mCrimes.get(position);
+//                return CrimeFragment.newInstance(crime.getID());
+//            }
+//
+//            @Override
+//            public int getCount() {
+//                return mCrimes.size();
+//            }
+//        });
+
+        // For Challenge
+        mAdapter = new FragmentStatePagerAdapter(fragmentManager) {
             @Override
             public Fragment getItem(int position) {
                 Crime crime = mCrimes.get(position);
@@ -47,7 +70,10 @@ public class CrimePagerActivity extends AppCompatActivity {
             public int getCount() {
                 return mCrimes.size();
             }
-        });
+        };
+
+        // For Challenge
+        mViewPager.setAdapter(mAdapter);
 
         for (int i = 0; i < mCrimes.size(); i++) {
             if (mCrimes.get(i).getID().equals(crimeId)) {
@@ -55,5 +81,21 @@ public class CrimePagerActivity extends AppCompatActivity {
                 break;
             }
         }
+
+        // For Challenge
+        mFirstButton = findViewById(R.id.jump_to_first);
+        mFirstButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mViewPager.setCurrentItem(0);
+            }
+        });
+        mLastButton = findViewById(R.id.jump_to_last);
+        mLastButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mViewPager.setCurrentItem(mCrimes.size() - 1);
+            }
+        });
     }
 }
